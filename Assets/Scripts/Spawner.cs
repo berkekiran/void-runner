e1ca6e73718e304cf38260a3fff7ecb051aee67d
burakhammusoglu <mburak.hammusoglu@gmail.com>
2020-07-21 01:10:51 +0300

    basic ship movement and endless tunnel

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject tunnel;
+    public List<GameObject> roadIns;
+    public GameObject ship;
+    int valueZ;
+
+    void Start()
+    {
+
+        valueZ = -25;
+        for (int i = 0; i <10; i++) AddRoad();
+
+    }
+
+    void Update()
+    {
+
+        if (ship.transform.position.z > (valueZ - 25 * 2))
+        {
+            AddRoad();
+            Destroy(roadIns[0]);
+            roadIns.Remove(roadIns[0]);
+        }
+
+    }
+    
+
+    public void AddRoad()
+    {
+
+        valueZ = valueZ + 25;
+        roadIns.Add(Instantiate(tunnel, new Vector3(0, 0, valueZ), tunnel.transform.rotation));
+
+    }
+}
---
+++ Assets/Scripts/spaceShipMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceShipMovement : MonoBehaviour
+{
+    public float hiz = 0.5f;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 vector = new Vector3(horizontal, 0, vertical);
+        rb.AddForce(vector * hiz);
+
+        
+    }
+}
