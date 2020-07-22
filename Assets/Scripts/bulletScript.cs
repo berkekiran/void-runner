c5e3ecf69c14b9f05a9591c36aa095fd0a32c926
burakhammusoglu <mburak.hammusoglu@gmail.com>
2020-07-22 18:57:55 +0300

    button control and fire

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public float hiz = 3f;
+    void Start()
+    {
+        Destroy(gameObject, 4f);
+    }
+
+    
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(0, 0, hiz*Time.deltaTime);
+    }
+}
--- Assets/Scripts/spaceShipMovement.cs
+++ Assets/Scripts/spaceShipMovement.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpaceShipMovement : MonoBehaviour
+public class spaceShipMovement : MonoBehaviour
 {
+    Rigidbody rb;
+    int guc = 1;
+    public Transform leftFirePosition;
+    public Transform rightFirePosition;
+    public GameObject bullet;
     public float hiz = 0.5f;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     
     void Update()
     {
         this.GetComponent<Rigidbody>().MovePosition(transform.position + Vector3.forward * hiz);
+        
     }
+
+    public void left()
+    {
+        
+        transform.position += new Vector3(-guc, 0, 0);
+    }
+    public void right()
+    {
+        transform.position += new Vector3(guc, 0, 0);
+    }
+    public void fire()
+    {
+        Instantiate(bullet, rightFirePosition.transform.position, new Quaternion());
+        Instantiate(bullet, leftFirePosition.transform.position, new Quaternion());
+    }
+
 }
